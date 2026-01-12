2140cb846429178709696a4712ddc793c3b4be34
lcotog <lcotog@iesrafaelalberti>
2026-01-12 17:39:29 +0100

    lo que sea

--- 3.0.9.py
+++ 3.0.9.py
@@ -1,4 +1,6 @@
 #Mostrar la posición que ocupa cada letra dentro de la cadena
 cadena="hola, buenos dias"
+contador=0
 for letra in cadena:
-    print(letra,cadena.find(letra))dawda
+    print(letra, contador)
+    contador+=1
